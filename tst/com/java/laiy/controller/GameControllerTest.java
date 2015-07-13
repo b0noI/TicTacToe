4aa3f526c453b52c8a75dece195d841f8a49ccc1
Alex <hattkis@gmail.com>
2015-07-13 15:53:53 +0300

    GameControllerTest added

---
+++ tst/com/java/laiy/controller/GameControllerTest.java
@@ -0,0 +1,38 @@
+package com.java.laiy.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameControllerTest {
+
+    @Test
+    public void testGetWinner() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGameName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBoard() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentPlayer() throws Exception {
+
+    }
+}
