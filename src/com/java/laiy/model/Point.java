e4942ef767018df20f7e2d3d6a847d9e9cc016e9
Alex <hattkis@gmail.com>
2015-07-14 14:30:26 +0300

    Point class added

---
+++ src/com/java/laiy/model/Point.java
@@ -0,0 +1,20 @@
+package com.java.laiy.model;
+
+public class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
