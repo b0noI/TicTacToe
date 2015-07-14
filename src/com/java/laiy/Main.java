a5a7826102673af7c3c106de221dcca290c7fb1f
Alex <hattkis@gmail.com>
2015-07-14 17:10:24 +0300

    Main updated

--- src/com/java/laiy/Main.java
+++ src/com/java/laiy/Main.java
@@ -4,25 +4,24 @@ import com.java.laiy.controller.GameController;
 import com.java.laiy.model.Board;
 import com.java.laiy.model.Figure;
 import com.java.laiy.model.Player;
+import com.java.laiy.model.exceptions.InvalidPointException;
+import com.java.laiy.model.exceptions.PointOccupiedException;
 import com.java.laiy.view.ConsoleView;
 
 public class Main {
 
-    public static void main(final String[] args) {
-
+    public static void main(final String[] args) throws InvalidPointException, PointOccupiedException {
+        final String GAME_NAME = "XO";
         final Board board = new Board();
         final Player[] players = new Player[2];
-        players[0] = new Player("Ox", Figure.X);
-        players[1] = new Player("Xo", Figure.O);
-        final GameController game = new GameController("string for the name of the game", players, board);
-        final ConsoleView twoConsoleView = new ConsoleView(game);
-        final ConsoleView consoleView = new ConsoleView(game);
-        startGame(twoConsoleView);
+        players[0] = new Player("Xonstantin", Figure.X);
+        players[1] = new Player("Oleg", Figure.O);
+        final GameController gameController = new GameController(GAME_NAME, players, board);
+        final ConsoleView consoleView = new ConsoleView(gameController);
+        final Game game = new Game(consoleView,gameController);
+
+        game.theGame();
     }
 
-    private static void startGame(final ConsoleView consoleView) {
-        consoleView.showGameName();
-        consoleView.showPlayers();
-    }
 
 }
