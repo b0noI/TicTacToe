6db5b42f5263e9a4b499603e86ca9cf71de2ac34
Alex <hattkis@gmail.com>
2015-07-24 16:20:04 +0300

    ConsoleMenuView updated

--- src/com/java/laiy/view/ConsoleMenuView.java
+++ src/com/java/laiy/view/ConsoleMenuView.java
@@ -5,7 +5,10 @@ import com.java.laiy.controller.GameController;
 import com.java.laiy.model.Board;
 import com.java.laiy.model.Figure;
 import com.java.laiy.model.Player;
+import com.java.laiy.model.exceptions.GoodButBadBoardSizeException;
+import com.java.laiy.model.exceptions.InvalidBoardSizeException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ConsoleMenuView {
@@ -17,6 +20,12 @@ public class ConsoleMenuView {
     private static final int SETTINGS_CODE = 3;
 
     private static final int EXIT_CODE = 4;
+
+    private static int BOARD_SIZE;
+
+    private static final int MIN_SIZE = 3;
+
+    private static final int WRONG_SIZE = 0;
 
     public static void showMenuWithResult() {
 
@@ -51,35 +60,64 @@ public class ConsoleMenuView {
         }
     }
 
-    private static void defaultStart(){
-        final String gameName = "XO";
-        final int boardSize = 3;
-        final String playerOneName = "PLAYER X";
-        final String playerTwoName = "PLAYER O";
-        customStart(boardSize,playerOneName,playerTwoName, gameName);
+    public static int getSize(){
+        return BOARD_SIZE;
     }
 
-    private static void customInput(){
+    private static void defaultStart(){
+        final String gameName = "XO";
+        BOARD_SIZE = 3;
+        final String playerOneName = "PLAYER X";
+        final String playerTwoName = "PLAYER O";
+        customStart(BOARD_SIZE,playerOneName,playerTwoName, gameName);
+    }
+
+    private static void customInput() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter game name:");
-        String gameName = input.nextLine();
+        final String gameName = "XO";
         System.out.println("Enter player one name:");
         String playerOneName = input.nextLine();
         System.out.println("Enter player two name:");
         String playerTwoName = input.nextLine();
-        System.out.println("Enter board size:");
-        int boardSize = input.nextInt();
+        final int boardSize = enterSize();
         customStart(boardSize,playerOneName,playerTwoName, gameName);
     }
 
-    private static void customStart(final int boardSize, final String playerOneName, final String playerTwoName, final String gameName){
-        final Board board = new Board(boardSize);
+    private static int enterSize(){
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.println("Enter board size:");
+            BOARD_SIZE = input.nextInt();
+            if (BOARD_SIZE <= WRONG_SIZE){
+                throw new InvalidBoardSizeException();
+            }
+            if (BOARD_SIZE > WRONG_SIZE && BOARD_SIZE < MIN_SIZE){
+                throw new GoodButBadBoardSizeException();
+            }
+
+        }
+        catch (final InputMismatchException | InvalidBoardSizeException e){
+            System.out.println("Input is wrong, please enter correct integer greater than zero");
+            enterSize();
+        }
+        catch (final GoodButBadBoardSizeException e){
+            System.out.println("Size must be equal or greater than 3, please try again");
+            enterSize();
+        }
+        return BOARD_SIZE;
+    }
+
+    private static void customStart(final int BOARD_SIZE, final String playerOneName, final String playerTwoName, final String gameName){
+        final Board board = new Board(BOARD_SIZE);
         final Player[] players = new Player[2];
         players[0] = new Player(playerOneName, Figure.X);
         players[1] = new Player(playerTwoName, Figure.O);
         final GameController gameController = new GameController(gameName, players, board);
         final ConsoleView consoleView = new ConsoleView(gameController);
         final Game game = new Game(consoleView,gameController);
+        consoleView.showGameName();
+        consoleView.showPlayers();
         game.theGame();
     }
+
 }
