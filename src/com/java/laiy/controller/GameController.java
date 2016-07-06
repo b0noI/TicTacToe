061b9e27e8d22c68905c89eacdb890d189e1ae9b
Artyom Ganev <a.a.ganev@gmail.com>
2016-07-06 19:06:06 +0300

    GameController() small refactoring

--- src/com/java/laiy/controller/GameController.java
+++ src/com/java/laiy/controller/GameController.java
@@ -26,6 +26,8 @@ public class GameController {
 
     private final Board board;
 
+    private final int boardLength;
+
     public GameController(final String gameName, final Player[] players, final Board board) {
         if (gameName == null || gameName.isEmpty()) {
             this.gameName = GAME_NAME;
@@ -34,20 +36,20 @@ public class GameController {
         }
         this.board = board;
         this.players = players;
+        this.boardLength = board.getFiguresArray().length;
     }
 
     public Player getWinner() {
-        for ( Player playerForCheck : players) {
-            if (checkRowsForWin(playerForCheck.getFigure().toString()) ||
-                checkLinesForWin(playerForCheck.getFigure().toString()) ||
-                checkDiagsForWin(playerForCheck.getFigure().toString())) {
+        for (Player playerForCheck : players) {
+            String figureToCheck = playerForCheck.getFigure().toString();
+            if (checkRowsForWin(figureToCheck) || checkLinesForWin(figureToCheck) || checkDiagsForWin(figureToCheck)) {
                 return playerForCheck;
             }
         }
         return null;
     }
 
-    public  String getGameName() {
+    public String getGameName() {
         return gameName;
     }
 
@@ -61,10 +63,9 @@ public class GameController {
 
     public void move(final int x, final int y, final Player player) throws PointOccupiedException {
         try {
-            if (board.getFigure(x,y) != null) {
+            if (board.getFigure(x, y) != null) {
                 throw new PointOccupiedException();
-            }
-            else {
+            } else {
                 try {
                     board.setFigure(x, y, player.getFigure());
                 } catch (InvalidPointException e) {
@@ -76,9 +77,9 @@ public class GameController {
         }
     }
 
-    public boolean getNextTurn(){
-        final Figure[][] figures =  getBoard().getFiguresArray();
-        if (getWinner() != null){
+    public boolean getNextTurn() {
+        final Figure[][] figures = getBoard().getFiguresArray();
+        if (getWinner() != null) {
             return false;
         }
         for (Figure[] figureArray : figures) {
@@ -97,29 +98,25 @@ public class GameController {
 
         if (playersTurns[FIRST_PLAYER] == playersTurns[SECOND_PLAYER]) {
             return players[firstPlayerNum];
-        }
-        else {
-            if ( firstPlayerNum  == FIRST_PLAYER ) {
-                return players[SECOND_PLAYER];
-            }
-            else {
-                return players[FIRST_PLAYER];
-            }
+        } else if (firstPlayerNum == FIRST_PLAYER) {
+            return players[SECOND_PLAYER];
+        } else {
+            return players[FIRST_PLAYER];
         }
     }
 
     private boolean checkLinesForWin(final String playerFigure) {
-        for (int row = 0 ; row < board.getFiguresArray().length ; row++) {
-                if ( getPlayerForLine(row, playerFigure) == board.getFiguresArray().length) {
-                    return true;
-                }
+        for (int row = 0; row < boardLength; row++) {
+            if (getPlayerForLine(row, playerFigure) == boardLength) {
+                return true;
+            }
         }
         return false;
     }
 
     private boolean checkRowsForWin(final String playerFigure) {
-        for (int column = 0 ; column < board.getFiguresArray().length ; column++) {
-            if ( getPlayerForColumn(column, playerFigure) == board.getFiguresArray().length) {
+        for (int column = 0; column < boardLength; column++) {
+            if (getPlayerForColumn(column, playerFigure) == boardLength) {
                 return true;
             }
         }
@@ -127,8 +124,8 @@ public class GameController {
     }
 
     private boolean checkDiagsForWin(final String playerFigure) {
-        for (int diag_direction = 0 ; diag_direction < DIAGS_FOR_CHECK ; diag_direction++) {
-            if ( getPlayerForDiag(diag_direction, playerFigure) == board.getFiguresArray().length) {
+        for (int diag_direction = 0; diag_direction < DIAGS_FOR_CHECK; diag_direction++) {
+            if (getPlayerForDiag(diag_direction, playerFigure) == boardLength) {
                 return true;
             }
         }
@@ -138,20 +135,22 @@ public class GameController {
     private int getPlayerForDiag(final int direction, final String playerFigure) {
         int playerDiagCount = 0;
         try {
-        if ( direction == DIAG_UP ) {
-            for (int i = 0; i < board.getFiguresArray().length ; i++) {
-                if ((board.getFigure(i, i) != null) && (board.getFigure(i, i).toString().equals(playerFigure) )) {
+            if (direction == DIAG_UP) {
+                for (int i = 0; i < boardLength; i++) {
+                    if ((board.getFigure(i, i) != null) && equalsFigures(i, i, playerFigure)) {
                         playerDiagCount++;
                     }
+                }
             }
-        }
-        if ( direction == DIAG_DOWN ) {
-            for (int i = 0; i < board.getFiguresArray().length ; i++) {
-                    if ((board.getFigure((board.getFiguresArray().length - 1) - i, i) != null) && (board.getFigure((board.getFiguresArray().length - 1) - i, i).toString().equals(playerFigure))) {
+            if (direction == DIAG_DOWN) {
+                for (int i = 0; i < boardLength; i++) {
+                    int row = (boardLength - 1) - i;
+                    if ((board.getFigure(row, i) != null) && equalsFigures(row, i, playerFigure)) {
                         playerDiagCount++;
                     }
+                }
             }
-        }} catch (InvalidPointException e) {
+        } catch (InvalidPointException e) {
             e.printStackTrace();
         }
         return playerDiagCount;
@@ -159,9 +158,9 @@ public class GameController {
 
     private int getPlayerForColumn(final int column, final String playerFigure) {
         int playerColumnCount = 0;
-        for (int i = 0; i < board.getFiguresArray().length ; i++) {
+        for (int i = 0; i < boardLength; i++) {
             try {
-                if ((board.getFigure(i, column) != null) && (board.getFigure(i, column).toString().equals(playerFigure))) {
+                if ((board.getFigure(i, column) != null) && equalsFigures(i, column, playerFigure)) {
                     playerColumnCount++;
                 }
             } catch (InvalidPointException e) {
@@ -173,15 +172,15 @@ public class GameController {
 
     private int[] getPlayersTurns() {
         int[] playersTurns = new int[players.length];
-        for ( int playerNum = 0 ; playerNum < players.length ; playerNum++) {
-                playersTurns[playerNum] = getPlayerForBoard(players[playerNum].getFigure().toString());
+        for (int playerNum = 0; playerNum < players.length; playerNum++) {
+            playersTurns[playerNum] = getPlayerForBoard(players[playerNum].getFigure().toString());
         }
         return playersTurns;
     }
 
     private int getPlayerForBoard(final String playerFigure) {
         int playerBoardCount = 0;
-        for (int i = 0 ; i < board.getFiguresArray().length ; i++) {
+        for (int i = 0; i < boardLength; i++) {
             playerBoardCount += getPlayerForLine(i, playerFigure);
         }
         return playerBoardCount;
@@ -189,9 +188,9 @@ public class GameController {
 
     private int getPlayerForLine(final int row, final String playerFigure) {
         int playerRowCount = 0;
-        for (int i = 0; i < board.getRowLength(row) ; i++) {
+        for (int i = 0; i < board.getRowLength(row); i++) {
             try {
-                if ((board.getFigure(row, i) != null) && (board.getFigure(row, i).toString().equals(playerFigure))) {
+                if ((board.getFigure(row, i) != null) && equalsFigures(row, i, playerFigure)) {
                     playerRowCount++;
                 }
             } catch (InvalidPointException e) {
@@ -202,8 +201,8 @@ public class GameController {
     }
 
     private int getFirstPlayerNum(final Player firstPlayer) {
-        int firstPlayerPositionNum  = 0;
-        for ( int num = 0 ; num < players.length ; num++) {
+        int firstPlayerPositionNum = 0;
+        for (int num = 0; num < players.length; num++) {
             if (players[num] == firstPlayer) {
                 firstPlayerPositionNum = num;
             }
@@ -211,4 +210,7 @@ public class GameController {
         return firstPlayerPositionNum;
     }
 
+    private boolean equalsFigures(final int x, final int y, final String playerFigure) throws InvalidPointException {
+        return board.getFigure(x, y).toString().equals(playerFigure);
+    }
 }
